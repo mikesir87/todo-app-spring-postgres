354638a0bec9ad4f53ad95ee60180ed2b614461e
shelajev <shelajev@gmail.com>
2024-09-23 15:59:51 +0300

    fix vendor! dockerignore ignored them

--- src/main/resources/public/js/vendor/backbone.js
+++ src/main/resources/public/js/vendor/backbone.js
@@ -9,7 +9,7 @@
 
   // Set up Backbone appropriately for the environment. Start with AMD.
   if (typeof define === 'function' && define.amd) {
-    define(['public/js/underscore', 'public/js/jquery', 'exports'], function(_, $, exports) {
+    define(['public/js/vendor/underscore', 'public/js/vendor/jquery', 'exports'], function(_, $, exports) {
       // Export global even in AMD case in case this script is loaded with
       // others that may still expect a global Backbone.
       root.Backbone = factory(root, exports, _, $);
@@ -17,7 +17,7 @@
 
   // Next for Node.js or CommonJS. jQuery may not be needed as a module.
   } else if (typeof exports !== 'undefined') {
-    var _ = require('public/js/underscore');
+    var _ = require('public/js/vendor/underscore');
     factory(root, exports, _);
 
   // Finally, as a browser global.

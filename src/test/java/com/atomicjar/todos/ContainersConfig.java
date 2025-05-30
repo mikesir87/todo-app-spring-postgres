7c94af1000d0ccc2722bc08a96a8faa548b4c3e5
Michael Irwin <mikesir87@gmail.com>
2025-05-30 10:21:15 -0400

    Allow postgres image to be configurable

--- src/test/java/com/atomicjar/todos/ContainersConfig.java
+++ src/test/java/com/atomicjar/todos/ContainersConfig.java
@@ -1,10 +1,12 @@
 package com.atomicjar.todos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
@@ -12,9 +14,10 @@ public class ContainersConfig {
     @Bean
     @ServiceConnection
     @RestartScope
-    PostgreSQLContainer<?> postgreSQLContainer() {
+    PostgreSQLContainer<?> postgreSQLContainer(@Value("${docker.image.postgres}") String postgresImage) {
         return new PostgreSQLContainer<>(
-                "postgres:16-alpine");
+                DockerImageName.parse(postgresImage).asCompatibleSubstituteFor("postgres")
+        );
     }
 
 }
